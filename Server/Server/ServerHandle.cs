d00309306760fbf8fa48e09ed2b185740236a1fb
LucasDissie <l.k.disseldorp@students.uu.nl>
2021-10-15 14:05:31 +0200

    Added Server

---
+++ Server/Server/ServerHandle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Numerics;
+using System.Text;
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+            bool seeker = _packet.ReadBool();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SendIntoGame(_username, seeker);
+        }
+
+        public static void PlayerMovement(int _fromClient, Packet _packet)
+        {
+            bool[] _inputs = new bool[_packet.ReadInt()];
+            for (int i = 0; i < _inputs.Length; i++)
+            {
+                _inputs[i] = _packet.ReadBool();
+            }
+            Quaternion _rotation = _packet.ReadQuaternion();
+
+            Server.clients[_fromClient].player.SetInput(_inputs, _rotation);
+        }
+        public static void ClientGeoCoordinate(int _fromClient, Packet _packet)
+        {
+            GeoCoordinate coordinate = _packet.ReadGeoCoordinate();
+            
+            
+
+            Server.clients[_fromClient].player.position = new Vector3((float)coordinate.Latitude, 0, (float)coordinate.Longitude);
+        }
+    }
+}
